d0f0fe1d49940faf71eae46025713152d1aacfee
arunhistory <garigarimegane625@gmail.com>
2026-08-28 01:39:38 +0900

    Export repetition-aware WebAssembly search ABI

--- scripts/build-wasm.sh
+++ scripts/build-wasm.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 set -eu
 mkdir -p public/wasm
-emcc cpp/engine.cpp \
+emcc cpp/engine_history.cpp \
   -O3 \
   -std=c++17 \
   -include initializer_list \
@@ -11,6 +11,8 @@ emcc cpp/engine.cpp \
   -Wl,--export=shogi_engine_version \
   -Wl,--export=shogi_input_buffer \
   -Wl,--export=shogi_input_capacity \
+  -Wl,--export=shogi_history_buffer \
+  -Wl,--export=shogi_history_capacity \
   -Wl,--export=shogi_evaluate_material_codes \
   -Wl,--export=shogi_evaluate_material \
   -Wl,--export=shogi_evaluate_position \
@@ -18,6 +20,8 @@ emcc cpp/engine.cpp \
   -Wl,--export=shogi_legal_move_at \
   -Wl,--export=shogi_is_check \
   -Wl,--export=shogi_is_mate \
+  -Wl,--export=shogi_repetition_status_with_history \
   -Wl,--export=shogi_search_best_move \
+  -Wl,--export=shogi_search_best_move_with_history \
   -Wl,--export=shogi_nodes_searched \
   -o public/wasm/shogi_engine.wasm
